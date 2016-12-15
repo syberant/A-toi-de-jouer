6241f918626a1b247a23c58966eebec4e0063b38
Splinter Suidman <splintersuidman@MBPvanSplinter.fritz.box>
2016-12-15 18:42:45 +0100

    Adds click event to add marker

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+// vars
 var coords = {
     lat: 48.857899,
     lng: 2.295111
 }
 var map;
 var panorama;
+var selectedCoords = {
+    lat: null,
+    lng: null
+}
 
+// initalises map
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: coords.lat, lng: coords.lng},
@@ -16,5 +22,24 @@ function initMap() {
         position: {lat: coords.lat, lng: coords.lng},
         pov: {heading: 165, pitch: 0},
         zoom: 1
+    });
+
+    // creates new marker
+    var selectedMarker = new google.maps.Marker({
+        draggable: true,
+        position: selectedCoords,
+        map: map,
+        title: "Selected location"
+    });
+
+    // click adds marker
+    google.maps.event.addListener(map, 'click', function (event) {
+            selectedCoords.lat = event.latLng.lat();
+            selectedCoords.lng = event.latLng.lng();
+            document.getElementById("lat").value = event.latLng.lat();
+            document.getElementById("lng").value = event.latLng.lng();
+            selectedMarker.setPosition(selectedCoords);
+            selectedMarker.setMap(map);
+
     });
 }
