e6a0eb20d1624c6ec7dca1666ca921f2e7d95a76
syberant <sybrand.s.y.b@gmail.com>
2016-12-22 13:55:50 +0100

    Create calculateScore.js

---
+++ calculateScore.js
@@ -0,0 +1,9 @@
+function calculateSingleScore(singleScore) {
+  return 1 / Number(singleScore) * 10000;
+}
+
+function calculateScore(Scores) {
+  for (var i = 0; i++; i < Scores.length) {
+    Scores[i] = calculateSingleScore(Scores[i]);
+  }
+}
